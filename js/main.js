5946001ee2e3bb7c6aef66cd483e34fdd930fb20
danielmshawn <danielmshawn@gmail.com>
2023-01-20 17:00:18 -0800

    added wireframe, pseudocode.txt, and began to add html elements

---
+++ js/main.js
@@ -0,0 +1,20 @@
+ /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+ // The randomized winning code 
+let rndCode;
+// # turn so game knows which row on board to render
+let turn;
+// #/4  circle player is choosing in order to pick colors accurately
+let choiceNum
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
