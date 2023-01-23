f4b256f3728437939c101849e48a497282f7afe9
danielmshawn <danielmshawn@gmail.com>
2023-01-23 14:21:00 -0800

    Added Javascript that changes the messages and visiblility of play again button depending on game state

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@
   '6': 'grey'
  };
 
+ const PEGS = {
+  '0': 'white',
+  '1': 'maroon', //correct color but not correct position
+  '2': 'black'   // correct color AND correct position
+ }
 
 
 
@@ -18,11 +23,13 @@ let board; //will be an array of 12 row arrays w 4 cells
 let rndCode; // randomized winning code
 let turn; // turn # so game knows which row on board to render
 let winner; // is playerChoice === rndCode?
+let playerGuess;
 
 
 
   /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h3');
+const playAgainBtn = document.getElementById('play-again');
 
   /*----- event listeners -----*/
 
@@ -72,12 +79,12 @@ function init() {
 
 //visualize all state in the DOM
 function render() {
-renderBoard();
-renderPegs();
-renderMessage();
+  renderBoard();
+  renderPegs();
+  renderMessage();
 
-//Hide/show UI elements ( play again button...also pegs? Seperate for pegs?
-renderControls();
+  //Hide/show UI elements ( play again button...also pegs? Seperate for pegs?
+  renderControls();
 
 
 }
@@ -89,9 +96,8 @@ function renderBoard() {
     rowArr.forEach(function(cellVal, colIdx) {
       const cellId = `r${rowIdx}c${colIdx}`;
       const cellEl = document.getElementById(cellId);
-      cellEl.style.backgroundColor = 'purple';
+      cellEl.style.backgroundColor = COLORS[cellVal];
     });
-
   });
 }
 
@@ -101,8 +107,8 @@ function renderPegs() {
     rowArr.forEach(function(pegVal, colIdx) {
       const pegId = `r${rowIdx}peg${colIdx}`;
       const pegEl = document.getElementById(pegId);
-      pegEl.style.backgroundColor = 'red';
-      console.log(pegEl);
+      pegEl.style.backgroundColor = PEGS[pegVal];
+    
     });
   
   });
@@ -114,11 +120,22 @@ function renderPegs() {
 
 
 function renderMessage() {
-
+  if(turn > 11) {
+    messageEl.innerText = "Sorry, you've run out of choices!";
+  } else if (playerGuess === rndCode) {
+    messageEl.innerText = "Congratulations! You chose the right mushrooms!"
+  } else {
+    //Message saying # of maroon pegs in row + # of black pegs in row. 
+    //Will need to do a forEach loop that counts how many 1's, then 2's
+    //In the specfic row's peg array?
+    messageEl.innerHTML = `<span style="color: maroon"></span> : Right color<br>
+    <span style="color: black"></span> : Right position & color`;
+  }
 }
 
 function renderControls() {
-
+//Play Again button only visible when game ends 
+playAgainBtn.style.visibility = (turn > 11 || playerGuess === rndCode) ? 'visible': 'hidden';
 }
 
 
