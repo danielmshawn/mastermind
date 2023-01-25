7460caaf2f61bff4ea7b604ae6a8d76a6cc97fb2
danielmshawn <danielmshawn@gmail.com>
2023-01-25 09:15:33 -0800

    added selectors to renderControl, other misc cleanup

--- js/main.js
+++ js/main.js
@@ -1,102 +1,110 @@
-// TO DOs - make markers disappear when game ends. 
-// Replace "_correct" h3 with hidden secret code.
+// TO DOs - 2 biggies...
+// Game not ending when I run out of turns.
+// Play again button isnt resetting board :/
 // Figure out a place for a 'reference' for pegs? How does the player know what they mean?
+// Hover effects?
+// Change markers. At LEAST flip triangles, but something else maybe. Frogs? lol
+// Also...will colors replacing with images...that will make code no longer work, right? Becuase of colors.
+// MushroomMind...a "Toad"breaking game
 
 /*----- constants -----*/
-
- const COLORS = ['red', 'blue', 'yellow','green', 'black', 'grey']
-
- const PEGS = ['grey', 'black'];
+const COLORS = ['red', 'blue', 'yellow', 'green', 'black', 'grey']
+const PEGS = ['grey', 'black'];
 
 
 
-  /*----- state variables -----*/
+/*----- state variables -----*/
 let board;            //Represents the row where the player will make their guess.
 let secretCode;       // randomized winning code
 let turn;             // turn # so game knows which row (array) on the board to modify and render
-let winner; 
+let winner;
 let colorChoice;      // variable for choosing color picker
-let correctColor;     
+let correctColor;
 let correctPosition;
+let secretCodeDisplayArr;
 
 
 
-
-  /*----- cached elements  -----*/
-const messageEl = document.querySelector('h3');
+/*----- cached elements  -----*/
+const messageEl = document.querySelector('h2');
 const playAgainBtn = document.getElementById('play-again');
 const markerEls = [...document.querySelectorAll('#markers > div')];
 const submitBtn = document.getElementById("submit")
+const secretCodeAnswer = document.getElementById('secret-code');
 
-  /*----- event listeners -----*/
+
+
+/*----- event listeners -----*/
 document.getElementById('picker').addEventListener('click', handlePick);
 document.getElementById('markers').addEventListener('click', handleSelect);
 document.getElementById('submit').addEventListener('click', handleSubmit);
 document.getElementById('play-again').addEventListener('click', init);
 
-  /*----- functions -----*/
+
+
+/*----- functions -----*/
 init()
-  //Initialize all state, then call render()
-function init() { 
+//Initialize all state, then call render()
+function init() {
   board = [null, null, null, null]
   pegs = [];
   colorChoice = 'red';
-  secretCode = getSecretCode();
   turn = 0;
   winner = null;
+  secretCode = getSecretCode();
+  secretCodeDisplayArr = secretCode.map(color => color);
+  clearBoard();
   render();
-  
+
 }
 
 //visualize all state in the DOM
 function render() {
   renderColors();
   renderMessage();
-
-  //Hide/show UI elements ( play again button...also pegs? Seperate for pegs?
+  //Hide/show UI elements
   renderControls();
-
 }
-
 
 function renderColors() {
   board.forEach(function (color, idx) {
-    if(color) {
-      document.getElementById(`r${turn}c${idx}`).style.backgroundColor= `${color}`;
+    if (color) {
+      document.getElementById(`r${turn}c${idx}`).style.backgroundColor = `${color}`;
     }
   })
   pegs.forEach(function (color, idx) {
-    if(color) {
-      document.getElementById(`p${turn}c${idx}`).style.backgroundColor= `${color}`;
+    if (color) {
+      document.getElementById(`p${turn}c${idx}`).style.backgroundColor = `${color}`;
     }
   })
+  secretCodeDisplayArr.forEach(function (color, idx) {
+    if (color) {
+      document.getElementById(`secret${idx}`).style.backgroundColor = `${color}`;
+    }
+  })
+
 }
 
-
 function renderMessage() {
-  if(turn > 9) {
+  if (turn > 9) {
     messageEl.innerText = "Sorry, you've run out of guesses!";
   } else if (winner === 'winner') {
     messageEl.innerText = "Congratulations! You chose the right mushrooms!"
-  } else {
-    // Message saying # of black pegs in row + # of black pegs in row. 
-    // Will need to do a forEach loop that counts how many 1's, then 2's
-    // In the specfic row's peg array? Yeeeeeah this is now an ICEBOX
-    // messageEl.innerHTML = `<span style="color: maroon"></span> : Right color<br>
-    // <span style="color: black"></span> : Right position & color`;
   }
 }
 
 function renderControls() {
-//Play Again button only visible when game ends 
-playAgainBtn.style.visibility = (turn > 9 || winner === 'winner') ? 'visible': 'hidden';
-// Submit button disappears when game ends
-submitBtn.style.visibility = (turn > 9 || winner === 'winner') ? 'hidden': 'visible';
-//Need to hide markers here as well
-
+  //Play Again button only visible when game ends 
+  playAgainBtn.style.visibility = (turn > 9 || winner === 'winner') ? 'visible' : 'hidden';
+  // Submit button disappears when game ends
+  submitBtn.style.visibility = (turn > 9 || winner === 'winner') ? 'hidden' : 'visible';
+  //Need to hide markers here as well
+  markerEls.forEach(function (markerEl) {
+    const hideMarker = (turn > 9 || winner === 'winner');
+    markerEl.style.visibility = hideMarker ? 'hidden' : 'visible';
+  });
+  secretCodeAnswer.style.visibility = (turn > 9 || winner === 'winner') ? 'visible' : 'hidden';
 }
-
-
 
 // Player clicks a color, 
 function handlePick(evt) {
@@ -108,43 +116,37 @@ function handleSelect(evt) {
   render();
 }
 
-
-
 function handleSubmit(evt) {
-  if(board.includes(null)) return;
+  if (board.includes(null)) return;
   feedback();
   winner = checkWinner();
   render();
   updateForNextTurn();
 }
 
-
 function feedback() {
   let tempArr = board.map(color => color)
   let tempCode = secretCode.map(color => color)
-  tempArr.forEach(function(color, idx) {
-    if(color === tempCode[idx]) {
+  tempArr.forEach(function (color, idx) {
+    if (color === tempCode[idx]) {
       pegs.push('black');
       tempArr[idx] = 7;
       tempCode[idx] = 0;
     }
   })
-  tempArr.forEach(function(color, idx) {
-    if(tempCode.includes(color)) {
+  tempArr.forEach(function (color, idx) {
+    if (tempCode.includes(color)) {
       pegs.push('grey')
       tempArr[idx] = 7
       let colorIdx = tempCode.indexOf(color)
       tempCode[colorIdx] = 0
     }
   })
-  
 }
-
 
 function checkWinner() {
   if (pegs.every(color => color === 'black') && (pegs.length === 4)) return "winner";
   return null;
-
 }
 
 function updateForNextTurn() {
@@ -153,19 +155,16 @@ function updateForNextTurn() {
   turn = turn + 1;
 }
 
-
-
 function getSecretCode() {
   let codeArr = [];
-  for (let i=0; i < 4; i++) {
-    codeArr.push(COLORS[Math.floor(Math.random()*COLORS.length)])
+  for (let i = 0; i < 4; i++) {
+    codeArr.push(COLORS[Math.floor(Math.random() * COLORS.length)])
   }
   return codeArr;
+};
+
+function clearBoard() {
+// use for loop to `` to the proper rows and change all background color to white?
+
+
 }
-
-
-
-
-
-
-
